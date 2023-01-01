36d0c29778036e138a3bb03c3df5242d50fde37c
mdma <tiptop148859@gmail.com>
2023-01-01 15:54:06 +0500

    add datetime library
    add datetime library

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -2,20 +2,21 @@ import requests
 from dotenv import load_dotenv
 import os
 import additional_scripts
+import datetime
 
 
 def get_nasa_epic_image(api_key):
-    date = '2022-12-13'
+    user_date = datetime.datetime(year=2022, month=12, day=13) #'2022-12-13'
     payload = {'api_key': api_key}
-    url = f'https://api.nasa.gov/EPIC/api/natural/date/{date}'
+    url = f'https://api.nasa.gov/EPIC/api/natural/date/{user_date}'
     response = requests.get(url, params=payload)
     response.raise_for_status()
     content = response.json()
     image_links = []
     for description in content:
         image_name = description['image']
-        date = date.replace('-', '/')
-        url = f'https://api.nasa.gov/EPIC/archive/natural/{date}/png/{image_name}.png'
+        new_date = user_date.strftime("%Y/%m/%d")
+        url = f'https://api.nasa.gov/EPIC/archive/natural/{new_date}/png/{image_name}.png'
         image_links.append(url)
     for link_number, link in enumerate(image_links):
         format_file = additional_scripts.get_format_file(link)
